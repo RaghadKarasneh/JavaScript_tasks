49c248dc771c4494fe57a9af7ff0602ffcbcb96e
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-03-27 09:47:32 +0300

    Javascript Tasks

--- Review/app.js
+++ Review/app.js
@@ -9,26 +9,73 @@ Expected Output:
 
 
 function Arrays_sum(array1,array2){
-    let newArray=[];
-    let indexCount=0;
-
-    if(array1.length === 0) // to check that array1 is not empty
-    return 'array1 is empty';
-    if(array2.length ===0)
-    return 'array2 is empty'; // to check that array2 is not empty
-
-    while(indexCount<array1.length && indexCount<array2.length){ // to sum arrays index values
-        newArray.push(array1[indexCount]+array2[indexCount]);
-        indexCount ++;
-    }
-    if (indexCount === array1.length){ //to check that I add all array2 index values 
-        for (let i=indexCount; i<array2.length;i++){
-       
-            newArray.push(array2[i]);
-        }
-    }
+        let newArray=[];
+        let indexCount=0;
     
-    return newArray
-}
-console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
-document.write(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]))
+        while(indexCount<array1.length && indexCount<array2.length){ // to sum arrays index values
+            newArray.push(array1[indexCount]+array2[indexCount]);
+            indexCount ++;
+        }
+        if (indexCount === array1.length){ //to check that I add all array2 index values 
+            for (let i=indexCount; i<array2.length;i++){
+           
+                newArray.push(array2[i]);
+            }
+        }
+        if (indexCount === array2.length){ //to check that I add all array1 index values 
+            for (let i=indexCount; i<array1.length;i++){
+           
+                newArray.push(array1[i]);
+            }
+        }
+        
+        return newArray}
+console.log(Arrays_sum([1,0,2,3,4,5,6,7],[3,5,6,7,8,13]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Arrays_sum(array1,array2){
+//     let newArray=[];
+//     let indexCount=0;
+
+//     if(array1.length === 0) // to check that array1 is not empty
+//     return 'array1 is empty';
+//     if(array2.length ===0)
+//     return 'array2 is empty'; // to check that array2 is not empty
+
+//     while(indexCount<array1.length && indexCount<array2.length){ // to sum arrays index values
+//         newArray.push(array1[indexCount]+array2[indexCount]);
+//         indexCount ++;
+//     }
+//     if (indexCount === array1.length){ //to check that I add all array2 index values 
+//         for (let i=indexCount; i<array2.length;i++){
+       
+//             newArray.push(array2[i]);
+//         }
+//     }
+    
+//     return newArray
+// }
+// console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
+// document.write(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]))
