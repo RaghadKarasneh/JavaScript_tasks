4ca3e89e05a9debd2850cb7bf7703b5aa0f211ab
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-03-30 15:29:59 +0300

    JavaScript Tasks

--- test/app.js
+++ test/app.js
@@ -1,60 +1,31 @@
-/*let errorHandel= new Promise(function(resolve, reject){
-  let check=true;
-  if(check){
-    rescolve('correct')
-  }else{
-    reject(Error('Failed'))
-  }
-});
-
-errorHandel.then(function(resolve){
-  document.getElementById('result').innerHTML
-}).catch(function(reject){
-  console.log(reject);
-});
 
 
-*/
+let str='Welcom to our session! We are 3 Aqaba cohort 5';
 
 
+console.log(str.match(/a/ig));
+
+console.log(str.search(/a/i));
+console.log(str.replace(/[^a||e]/ig,'@'));
 
 
+console.log(str.match(/\d/ig));
 
+console.log(str.search(/\baqa/ig));
+///////////////////////////////////////////////
 
+/*Erro Handling */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let store =localStorage.setItem('color','red');
-//console.log(store);
-let x=localStorage.getItem('color');
-console.log(x);
-
-let obj={
-    name:'ali',
-    age:50
+function numbers(num){
+  if(typeof(num)!=='number')
+    return 'It is not  number';
+  else
+    return 'correct';
 }
 
-let js=JSON.stringify(obj);
-store=localStorage.setItem(obj,js);
-let y =window.localStorage.getItem(obj);
-console.log(y);
-*/
+console.log(numbers(5));
+
+
+
+
+
